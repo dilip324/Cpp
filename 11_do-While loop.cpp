be5fdb03f5416bbcd42b4c4fa0ab297ecc9ca38d
Dilip Prajapat <dilippprajapat80@gmail.com>
2025-11-28 17:37:16 +0530

    Multiplication of table by using  do-while loop.
    Write a C++ program to perform multiplication of table by using  do-while loop.

---
+++ 11_do-While loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //  multiplication of table using do while
+    int a = 5;
+    int i = 1;
+    do
+    {
+        cout<<a*i<<endl;
+        i++;
+    } while (i<=10);
+    
+    return 0;
+} 
