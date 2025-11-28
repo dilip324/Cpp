63e3a3ea0f30366a41c87d5e5de6440d219233f5
Dilip Prajapat <dilippprajapat80@gmail.com>
2025-11-28 17:49:00 +0530

    program of  break statement loop

---
+++ Break statement.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for (int i = 0; i < 40; i++)
+    {
+        /* code */
+        cout<<i<<endl;
+         if(i==2){
+            break; 
+         }
+            
+    } 
