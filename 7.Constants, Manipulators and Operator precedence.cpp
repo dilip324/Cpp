e9d673feec7c4c11abd7ea3c05f6f0e534127079
Dilip Prajapat <dilippprajapat80@gmail.com>
2025-11-28 15:32:27 +0530

    Add  C++ program on constants and operators
    writing a C++ program based on constants , manipulators and operators

---
+++ 7.Constants, Manipulators and Operator precedence.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    // int a=5;
+    // cout<<"the value of a was: "<<a;
+    // a=25;
+    // cout<<"the value of a is: "<<a;
+
+    // constants in c++
+//     const int a = 2;
+//     cout<<"the value of a was: "<<a<<endl;
+//     a = 45;   //you will get an error because 'a' is a constant
+// cout<<"the value of a is: "<<a<<endl; 
+
+//Manipulators in C++
+// int a = 4, b = 65, c=1234;
+// cout<<"the value of a without setw is:"<<a<<endl;
+// cout<<"the value of b without setw is:"<<b<<endl;
+// cout<<"the value of c without setw is:"<<c<<endl;
+
+// cout<<"the value of a is:"<<setw(8)<<a<<endl;
+// cout<<"the value of b is:"<<setw(4)<<b<<endl;
+// cout<<"the value of c is:"<<setw(4)<<c<<endl;
+
+// Operator precedence
+int a=3, b=4; 
+// int c = (a*5)+b;
+int c = ((((a*5)+b)-45)+87);
+cout<<c;
+ return 0;
+}  
