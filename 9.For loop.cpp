dc444f41f5152064ee8ce6891111aff62c0cff22
Dilip Prajapat <dilippprajapat80@gmail.com>
2025-11-28 16:13:08 +0530

    for loop by using multiplication of table
    writing a program for multiplication of table by using for loop.

---
+++ 9.For loop.cpp
@@ -0,0 +1,15 @@
+// By using for loop
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int i;
+    cout<<"give the number"<<endl;
+    cin>>n;
+    for(i=1;i<11;i++)
+    {
+    cout<<n*i<<endl; 
+    }
+    return 0; 
+} 
