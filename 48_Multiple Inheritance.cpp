a45199e2934410b635d93594760c0ce95ffa8f8f
Dilip Prajapat <dilippprajapat80@gmail.com>
2025-11-29 14:45:57 +0530

    An example of multiple inheritance in C++

---
+++ 48_Multiple Inheritance.cpp
@@ -0,0 +1,49 @@
+// Multiple Inheritance Syntax
+// class DerivedC: visibility-mode base1; visibility-mode base2
+// {
+//     Class body of class "DerivedC"
+// };
+
+
+#include<iostream>
+using namespace std;
+
+class Base1{
+    protected:
+    int base1int;
+
+    public:
+    void set_base1int(int a)
+    {
+        base1int = a;
+    }
+};
+
+class Base2{
+    protected:
+    int base2int;
+
+    public:
+    void set_base2int(int a)
+    {
+        base2int=a;
+    }
+};
+
+class Derived : public Base1, public Base2
+{
+    public:
+    void show(){
+        cout<<"The value of Base1 is "<<base1int<<endl;
+        cout<<"The value of Base2 is "<<base2int<<endl;
+        cout<<"The sum of these values is "<< base1int + base2int <<endl;
+    }
+};
+
+int main(){
+    Derived dilip;
+    dilip.set_base1int(25);
+    dilip.set_base2int(5);
+    dilip.show();
+    return 0;
+}
